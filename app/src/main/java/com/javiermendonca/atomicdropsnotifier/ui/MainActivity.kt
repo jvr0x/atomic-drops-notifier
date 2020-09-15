834d58a02f23337a7ca6f235b9fe04fc514553d5
Javier Mendonça <javiermendonca@protonmail.com>
2020-09-15 13:29:46 +0200

    Adding about menu

--- app/src/main/java/com/javiermendonca/atomicdropsnotifier/ui/MainActivity.kt
+++ app/src/main/java/com/javiermendonca/atomicdropsnotifier/ui/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.javiermendonca.atomicdropsnotifier.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.javiermendonca.atomicdropsnotifier.R
 import com.javiermendonca.atomicdropsnotifier.data.api.RetrofitBuilder
 import com.javiermendonca.atomicdropsnotifier.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,5 +37,24 @@ class MainActivity : AppCompatActivity() {
         binding.atomicDrops.layoutManager = GridLayoutManager(this, 1)
         binding.atomicDrops.adapter = adapter
         viewModel.atomicDrops.observe(this, { atomicDrops -> adapter.setAtomicDrops(atomicDrops) })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.about_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_show_about -> {
+                MaterialAlertDialogBuilder(this)
+                    .setTitle(getString(R.string.about))
+                    .setCancelable(true)
+                    .setMessage(getString(R.string.about_message, getString(R.string.app_name)))
+                    .setNeutralButton(android.R.string.ok) { _, _ -> }
+                    .show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
